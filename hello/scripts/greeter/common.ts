eb38b4536e3ddc786b1c0045ad1c7fbfe05b94f4
Medet Ahmetson <admin@blocklords.io>
2023-08-08 19:03:33 +0300

    Update the common.ts to use the superwallet testnet contract addresses

--- hello/scripts/greeter/common.ts
+++ hello/scripts/greeter/common.ts
@@ -10,42 +10,42 @@ let l2Signer = new ethers.Wallet(privateKey).connect(l2Provider)
 
 export function getType(chainId: number): string {
     if (chainId == 11155111) return "L1";
-    if (chainId == 42069) return "L2";
+    if (chainId == 535754) return "L2";
 
     throw `chain id ${chainId} unsupported\n`;
 }
 
 export function getOpposite(chainId: number): number {
-    if (chainId == 11155111) return 42069;
-    if (chainId == 42069) return 11155111;
+    if (chainId == 11155111) return 535754;
+    if (chainId == 535754) return 11155111;
 
     throw `chain id ${chainId} unsupported\n`;
 }
 
 export function getDeployTx(chainId: number): string {
     if (chainId == 11155111) return "0x486c110bf2e111ce30b6585c3c74665bc86559b1f48d03f3a6ce923b95771017";
-    if (chainId == 42069) return "0x0befb627bc4f23911d0e7c0be542e076a1fddd4eb6f8f89ceeb0236387035427";
+    if (chainId == 535754) return "0xb7e000f8e9c5ed940ee970fa17901fb242e45b8349faad0583cd8c2201ccd420";
 
     throw `chain id ${chainId} unsupported\n`;
 }
 
 export function getController(chainId: number): string {
     if (chainId == 11155111) return "0x0B7d8512a29BAd68d0df9805DC7Af6B74F54117B";
-    if (chainId == 42069) return "0xA4006F2f40341C7933591c959E724b9147A2f6C2";
+    if (chainId == 535754) return "0x0564C3e8Fe23c5A6220A300c303f41e43D9be9e2";
 
     throw `chain id ${chainId} unsupported\n`;
 }
 
 export function greeterAddress(chainId: number): string {
-    if (chainId == 11155111) return "0x3bD4aBCdC3e46205666C152e32B821370cE37EAD";
-    if (chainId == 42069) return "0xFe82ced1b9fDD79a07960503c832C6E534A3389E";
+    if (chainId == 11155111) return "0x14eEF697c9b0cE96ed0C06d20EcB338cd7BEA11a";
+    if (chainId == 535754) return "0xe40c7856B6D0e1B01dECBF9976BB706B9Cd1229f";
 
     throw `chain id ${chainId} unsupported\n`;
 }
 
 export function getSigner(chainId: number) {
     if (chainId == 11155111) return l1Signer;
-    if (chainId == 42069) return l2Signer;
+    if (chainId == 535754) return l2Signer;
 
     throw `chain id ${chainId} unsupported\n`;
 }
