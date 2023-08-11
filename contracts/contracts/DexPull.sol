751df4b882d1a78a78399310a747e325e3c3f4ae
Medet Ahmetson <admin@blocklords.io>
2023-08-11 19:53:45 +0300

    Refactoring DexPull to work

--- contracts/contracts/DexPull.sol
+++ contracts/contracts/DexPull.sol
@@ -3,30 +3,37 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import {IMessageRecipient} from "@hyperlane-xyz/core/contracts/interfaces/IMessageRecipient.sol";
-import {IInterchainSecurityModule, ISpecifiesInterchainSecurityModule} from "@hyperlane-xyz/core/contracts/interfaces/IInterchainSecurityModule.sol";
+import {HyperlaneConnectionClient} from "@hyperlane-xyz/core/contracts/HyperlaneConnectionClient.sol";
+import {TypeCasts} from "@hyperlane-xyz/core/contracts/libs/TypeCasts.sol";
 
-contract DexNotifier is Ownable, IMessageRecipient, ISpecifiesInterchainSecurityModule {
-    IInterchainSecurityModule public interchainSecurityModule;
+/**
+DexPull is the part of the dex that seats on the blockchains.
 
+It receives the message from a user to deduct the balance and that balance is send to the destination.
+It receives the message from mailbox, in that case it will execute the transaction.
+
+Deploy it with the SuperWallet Chain and DexPush parameters.
+*/
+contract DexPull is HyperlaneConnectionClient  {
     // The link from this blockchain to another address
     mapping(address => mapping(uint32 => address)) public superAccounts;
     mapping(address => uint256) public pools;
     mapping(address => mapping(uint32 => address)) public superTokens;
 
     bytes1 public addOp = 0x01;
+    uint32 public dexPushDest;
+    bytes32 public dexPush;
 
-    constructor(address mailbox) {
-//        __Router_initialize(mailbox);
+    // Mailbox is responsible for transmitting the messages
+    // Mailbox transmits the messages to the dexPush
+    constructor(address mailbox, address dexAddr, uint32 dexDest) {
+        __HyperlaneConnectionClient_initialize(mailbox);
+        dexPush = TypeCasts.addressToBytes32(dexAddr);
+        dexPushDest = dexDest;
     }
 
-//    add super accounts as a manager
-//    add super tokens as a manager
-//    deploy this script on SWT and on sepolia.
-
-
     /**
-     * @notice Transfer token from one chain to another using the dex pool.
+     * @notice Transfer token from one chain to another using the dex pool (middle chain).
      * @param destination the target chain id where the transaction should be executed
      * @param amount to transfer from this blockchain
      * @param token the token type
@@ -34,49 +41,44 @@ contract DexNotifier is Ownable, IMessageRecipient, ISpecifiesInterchainSecurity
     function transferToken(
         uint32 destination,
         uint256 amount,         // we take this amount
-        address token,          // of the tokens from the user in this blockchain.
-        address safeParamTo,             // safe sdk part to which smartcontract it needs to be send
-        bytes calldata safeParamData,     // safe sdk the transaction parameters
-        bytes calldata safeSignatures
+        address token          // of the tokens from the user in this blockchain.
     ) external {
         pools[token] += amount;
 
         require(IERC20(token).transferFrom(msg.sender, address(this), amount), "failed to get tokens");
 
-        // a smartcontract that keeps the pre-funded data. it's a safe wallet.
-        // we send the data to there.
-        address safeWallet = superAccounts[msg.sender][destination];
-        address tokenOnRemote = superTokens[token][destination];
+        // send it to the dex push.
+        // dex push should have the parameters of the request to send in destination.
+        //
+        //
+        bytes memory wrappedData = abi.encodePacked(addOp, msg.sender, token, amount);
 
-        // when DexNotifier on the destination handles it, it will send to proxyAddr(safe wallet) `amount` of `token`.
-        // then it will `executeTransaction`
-        bytes memory wrappedData = abi.encodePacked(addOp, safeWallet, tokenOnRemote, amount, safeParamTo, safeParamData, safeSignatures);
-
-//        _dispatch(destination, wrappedData);
-    }
-
-    function setInterchainSecurityModule(address _ism) external onlyOwner {
-        interchainSecurityModule = IInterchainSecurityModule(_ism);
+        mailbox.dispatch(dexPushDest, dexPush, wrappedData);
     }
 
     // ============ On receive functions ============
 
+    // The DexPull acts as the source
     function handle(uint32 _origin, bytes32 _sender, bytes calldata _message) external override {
         (bytes1 opType,
-            address proxyAddr,
+            address user,
             address token,
             uint256 amount,
             address safeParamTo,
             bytes memory safeParamData,
             bytes memory safeSignatures) = abi.decode(_message, (bytes1, address, address, uint256, address, bytes, bytes));
-        require(opType == addOp, "only add op, its for developers");
 
-        require(IERC20(token).transfer(proxyAddr, amount), "failed to send tokens to safe");
+        if (opType == addOp) {
+            continueTransfer(user, token, amount, safeParamTo, safeParamData, safeSignatures);
+        }
+    }
+
+    function continueTransfer(address user, address token, uint256 amount, address safeParamTo, bytes memory safeParamData, bytes memory safeSignatures) internal {
+        require(IERC20(token).transfer(user, amount), "failed to send tokens to safe");
 
         bytes memory payload = abi.encodeWithSignature("execTransaction(address,uint256,bytes,uint8,uint256,uint256,uint256,address,address,bytes",
             safeParamTo, 0, safeParamData, 1, 0, 0, 0, address(0), address(0), safeSignatures);
-        (bool success, bytes memory returnData) = proxyAddr.call(payload);
+        (bool success, bytes memory returnData) = user.call(payload);
         require(success);
     }
-
 }
