bb8ff8b6b3ba8ffb46e93c6dc4e95fb89aa5d4e8
Medet Ahmetson <admin@blocklords.io>
2023-08-13 02:09:38 +0300

    transfer params gets the data from contract artifacts

--- contracts/scripts/superwallet/transfer_params.ts
+++ contracts/scripts/superwallet/transfer_params.ts
@@ -1,3 +1,4 @@
+import {contractNetworks} from "../../../superwallet/lib/deployments";
 
 export const transferParams = {
     to: "0xd1d5b8Ea43f07e5e127D97652C9924Fa6372809a",
@@ -9,7 +10,7 @@ export const transferParams = {
 
     swtAcc: "0x80Cbc1f7fd60B7026C0088e5eD58Fc6Ce1180141",
 
-    sourceTokenAddr: "0x0564C3e8Fe23c5A6220A300c303f41e43D9be9e2",
-    destinationTokenAddr: "0xe40c7856B6D0e1B01dECBF9976BB706B9Cd1229f",
+    sourceTokenAddr: contractNetworks["goerli"].contracts.SuperWalletTest.address,
+    destinationTokenAddr: contractNetworks["baseTestnet"].contracts.SuperWalletTest.address,
     tokenId: 1,
 }
