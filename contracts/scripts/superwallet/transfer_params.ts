9dfa0023f34239bea610e848d5f5ceaf3224d057
Medet Ahmetson <admin@blocklords.io>
2023-08-13 01:30:10 +0300

    move the transfer params to it's own file

---
+++ contracts/scripts/superwallet/transfer_params.ts
@@ -0,0 +1,15 @@
+
+export const transferParams = {
+    to: "0xd1d5b8Ea43f07e5e127D97652C9924Fa6372809a",
+    totalAmount: 3,
+    sourceAmount: 1,
+    destinationNetworkName: "baseTestnet",
+    sourceNetworkName: "goerli",
+    swtNetworkName: "sepolia",
+
+    swtAcc: "0x80Cbc1f7fd60B7026C0088e5eD58Fc6Ce1180141",
+
+    sourceTokenAddr: "0x0564C3e8Fe23c5A6220A300c303f41e43D9be9e2",
+    destinationTokenAddr: "0xe40c7856B6D0e1B01dECBF9976BB706B9Cd1229f",
+    tokenId: 1,
+}
--- contracts/scripts/superwallet/transfer_part_0.ts
+++ contracts/scripts/superwallet/transfer_part_0.ts
@@ -9,25 +9,9 @@
  */
 
 // Registers the DexPulls
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
 import 'dotenv/config'
-import Safe, {EthersAdapter} from "@safe-global/protocol-kit";
-import {OperationType, SafeTransactionDataPartial} from "@safe-global/safe-core-sdk-types";
-
-export const transferParams = {
-    to: "0xd1d5b8Ea43f07e5e127D97652C9924Fa6372809a",
-    totalAmount: 3,
-    sourceAmount: 1,
-    destinationNetworkName: "baseTestnet",
-    sourceNetworkName: "goerli",
-    swtNetworkName: "sepolia",
-
-    swtAcc: "0x80Cbc1f7fd60B7026C0088e5eD58Fc6Ce1180141",
-
-    sourceTokenAddr: "0x0564C3e8Fe23c5A6220A300c303f41e43D9be9e2",
-    destinationTokenAddr: "0xe40c7856B6D0e1B01dECBF9976BB706B9Cd1229f",
-    tokenId: 1,
-}
+import {transferParams} from "./transfer_params";
 
 async function main() {
     // goerli
