ff0957bf258ce760a5f19bb2a16acd8ba669bf72
Medet Ahmetson <admin@blocklords.io>
2023-08-12 14:20:09 +0300

    add a script that sets the token

---
+++ contracts/scripts/superwallet/dexpush_supertoken.ts
@@ -0,0 +1,39 @@
+// This account sets a super user
+import { ethers } from "hardhat";
+import 'dotenv/config'
+async function main() {
+    let signers = await ethers.getSigners();
+    let signer = signers[0];
+
+    let DexPush = await ethers.getContractFactory("DexPush", signer)
+    let dexPushAddress = "0xB1A9a37543fB8919B17f0707d00857c6855BBDBf";
+
+
+    const swt = {
+        id: 1,
+        goerli: "0x0564C3e8Fe23c5A6220A300c303f41e43D9be9e2",
+        base: "0xe40c7856B6D0e1B01dECBF9976BB706B9Cd1229f",
+    }
+
+
+    let contract = await DexPush.attach(dexPushAddress);
+    const baseDomain = 84531;
+    const goerliDomain = 5;
+
+    console.log(`Setting ${swt.id} token in source ${baseDomain}...`);
+    let tx = await contract.setSuperToken(swt.id, baseDomain, swt.base);
+    console.log(`waiting...${tx.hash}`);
+    await tx.wait()
+    console.log(`Done!`);
+    console.log(`Setting ${swt.id} token in destination ${goerliDomain}...`);
+    let tx2 = await contract.setSuperToken(swt.id, goerliDomain, swt.goerli);
+    console.log(`waiting...${tx2.hash}`);
+    await tx2.wait()
+    console.log(`Done!`);
+    console.log(`User is now a super user (SU) in unix terms`);
+
+    return "done!"
+}
+
+
+main().then(console.log).catch(console.error);
