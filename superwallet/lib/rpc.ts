46f978b53fb7a79cf8000cae874c947c6af46057
Medet Ahmetson <admin@blocklords.io>
2023-08-12 12:13:27 +0300

    format the rpcs

--- superwallet/lib/rpc.ts
+++ superwallet/lib/rpc.ts
@@ -1,22 +1,22 @@
 export default {
     111551111: {
         url: process.env.NEXT_PUBLIC_L1_RPC!,
-        accounts: [process.env.NEXT_PUBLIC_ADMIN_PRIVATE_KEY!],
+        accounts: [process.env.NEXT_PUBLIC_PRIVATE_KEY!],
     },
     84531: {
-            url: "https://1rpc.io/base-goerli",
-            accounts: [process.env.NEXT_PUBLIC_ADMIN_PRIVATE_KEY],
+        url: "https://1rpc.io/base-goerli",
+        accounts: [process.env.NEXT_PUBLIC_PRIVATE_KEY],
     },
     999: {
-            url: "https://testnet.rpc.zora.energy",
-            accounts: [process.env.NEXT_PUBLIC_ADMIN_PRIVATE_KEY],
-        },
+        url: "https://testnet.rpc.zora.energy",
+        accounts: [process.env.NEXT_PUBLIC_PRIVATE_KEY],
+    },
     420: {
-            url: "https://goerli.optimism.io",
-            accounts: [process.env.NEXT_PUBLIC_ADMIN_PRIVATE_KEY],
+        url: "https://goerli.optimism.io",
+        accounts: [process.env.NEXT_PUBLIC_PRIVATE_KEY],
     },
     919: {
         url: "https://sepolia.mode.network",
-        accounts: [process.env.NEXT_PUBLIC_ADMIN_PRIVATE_KEY],
+        accounts: [process.env.NEXT_PUBLIC_PRIVATE_KEY],
     }
 };
