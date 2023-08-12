105febddb44f5e1d3a675b5ae03434f5522db442
Medet Ahmetson <admin@blocklords.io>
2023-08-12 13:54:22 +0300

    add mailbox to the constructor

--- contracts/deploy/05_dexpush.ts
+++ contracts/deploy/05_dexpush.ts
@@ -10,10 +10,12 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
     console.log(`Deployer: ${deployer}`);
 
+    // sepolia
+    const mailbox = "0xCC737a94FecaeC165AbCf12dED095BB13F037685";
 
     await deploy('DexPush', {
         from: deployer,
-        args: [],
+        args: [mailbox],
         log: true,
         autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
     });
