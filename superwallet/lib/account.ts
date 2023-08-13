c40e3fe1bc8026fc662306bbe00126574f9c06d8
Medet Ahmetson <admin@blocklords.io>
2023-08-13 10:24:30 +0300

    use optestnet

--- superwallet/lib/account.ts
+++ superwallet/lib/account.ts
@@ -1,5 +1,5 @@
 export const Account = {
-    baseTestnet: `0xEFe21201bdC8B3c2a659008F5806CE33eBC4dE45`,     // safe address
+    opTestnet: `0xEFe21201bdC8B3c2a659008F5806CE33eBC4dE45`,     // safe address
     sepolia: "0x80Cbc1f7fd60B7026C0088e5eD58Fc6Ce1180141",          // eoa address
     goerli: "0x07E5439921305486DF2505268349424E3886b647"    // safe address
 }
