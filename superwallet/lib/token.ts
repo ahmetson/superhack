8dc74696de9265341f3321b0e17f6ef4d867f8fc
Medet Ahmetson <admin@blocklords.io>
2023-08-13 09:02:50 +0300

    getting token by token name

--- superwallet/lib/token.ts
+++ superwallet/lib/token.ts
@@ -2,3 +2,11 @@ export const Tokens = [
     {symbol: "SWT", "name": "SuperWalletTest", id: 1, contracts: {}},
     {symbol: "TT", "name": "TestToken", id: 2, contracts: {}}
 ]
+
+export const tokenIdByName = (name: string): number => {
+    for (let token of Tokens) {
+        if (token.name === name) {
+            return token.id
+        }
+    }
+}
